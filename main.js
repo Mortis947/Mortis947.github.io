4ba607af1f21107f79e134a4cbcf2484531cbbd8
Mortis947 <2149775443@qq.com>
2026-04-25 11:21:26 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/1.png") 
+  {
+    myImage.setAttribute("src", "images/2.jpg");
+  } 
+  else if(mySrc==="images/2.jpg")
+  {
+    myImage.setAttribute("src", "images/3.jpg");
+  }
+  else{
+    myImage.setAttribute("src","images/1.png")
+  }
+};
+
+
+let myButton=document.querySelector("button");
+let myHeading=document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if(!myName){
+    setUserName();
+  }
+  else{
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Welcome to my website, ${myName}`;
+  }
+}
+
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Welcome to my website, ${storedName}`;
+}
+
+myButton.onclick = function () {
+  setUserName();
+};
